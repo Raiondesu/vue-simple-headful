67b53e2b932ea79566e99271bf88d51d57a22736
Raiondesu <a.raiondesu@gmail.com>
2018-03-30 15:21:27 +0300

    Removed types completely

--- index.ts
+++ index.ts
@@ -54,3 +54,18 @@ export default plugin;
 export interface Headful {
   [key: string]: any
 }
+
+declare module "vue/types/options" {
+  interface ComponentOptions<V extends Vue> {
+    headful?: Headful | {
+      (vm?: V): Headful
+    }
+  }
+}
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $headful<T extends object>(props: T): void
+    headful?: Headful
+  }
+}
--- types/test/plugin-test.ts
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import VueHeadful from '../../'
-
-Vue.use(VueHeadful);
